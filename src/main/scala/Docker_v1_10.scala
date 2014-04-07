471bfcf0d3d0e04f22a979e59f238761011a0854
Devon Stewart <blast@hardchee.se>
2014-04-06 17:49:29 -0700

    Adding "Common" trait to (attempt to) ease in request proxying

--- src/main/scala/Docker_v1_10.scala
+++ src/main/scala/Docker_v1_10.scala
@@ -1,10 +1,38 @@
 package se.hardchee.docker.api
 
+import scala.concurrent.duration._
+
+import akka.actor.ActorContext
+import akka.io.IO
+import akka.pattern.ask
+import akka.util.Timeout
+
+import spray.can.Http
+import spray.http.{HttpMethod, HttpRequest, HttpResponse, HttpEntity, Uri}
 import spray.routing.HttpServiceActor
 
 import se.hardchee.docker.api.types._
 import se.hardchee.docker.proxy.ProxyService
 import se.hardchee.docker_scala.converters.GoStringConverters
+
+trait Common {
+  self: HttpServiceActor =>
+
+  def makeRequest(method: HttpMethod, uri: Uri, entity: HttpEntity)(implicit context: ActorContext) = {
+    implicit val timeout: Timeout = 5.seconds
+    import context.{system, dispatcher}
+
+    onSuccess((IO(Http) ? HttpRequest(uri=uri, entity=entity)).mapTo[HttpResponse]) { resp =>
+      val strippedHeaders = resp.headers.filterNot({ h => h.name == "Content-Length" || h.name == "Date" || h.name == "Content-Type" })
+      complete(HttpResponse(
+        status=resp.status,
+        entity=resp.entity,
+        headers=strippedHeaders,
+        protocol=resp.protocol
+      ))
+    }
+  }
+}
 
 trait v1_10 extends HttpServiceActor with GoStringConverters {
   self: ProxyService =>
